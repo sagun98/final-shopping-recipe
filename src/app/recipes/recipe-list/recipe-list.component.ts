28aaab0f85f81f880ac97bd95d831f25e14ddfca
Sagun Maharjan <sagunmaharjann@Saguns-MBP.hsd1.la.comcast.net>
2018-09-03 12:11:58 -0500

    items added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test', 'A test desc', 'https://cdn.cnn.com/cnnnext/dam/assets/171027052520-processed-foods-exlarge-tease.jpg'),
+    new Recipe('Burger', 'A Burger desc', 'https://cdn.cnn.com/cnnnext/dam/assets/171027052520-processed-foods-exlarge-tease.jpg')
+ 
+  ]; 
   constructor() { }
 
   ngOnInit() {
